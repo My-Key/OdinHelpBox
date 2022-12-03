59e95a83402af7b0daebc1e7fc49cd0f34f2e734
Maciej <0my0key0@gmail.com>
2022-12-03 15:11:23 +0100

    Add documentation to attributes

--- HelpBox/HelpBoxAttribute.cs
+++ HelpBox/HelpBoxAttribute.cs
@@ -8,7 +8,12 @@ namespace OdinExtra.HelpBox
 		public string Text { get; private set; }
 
 		public string Details { get; private set; }
-
+		
+		/// <summary>
+		/// Draws compact help box
+		/// </summary>
+		/// <param name="text">Main text text. Can be a regular string or resolved to method that returns string to display</param>
+		/// <param name="details">Details text. Can be a regular string or resolved to method that returns string to display</param>
 		public HelpBoxAttribute(string text, string details = null)
 		{
 			Text = text;
--- HelpBox/TooltipDetailsAttribute.cs
+++ HelpBox/TooltipDetailsAttribute.cs
@@ -6,6 +6,10 @@ namespace OdinExtra.HelpBox
 	{
 		public string Details { get; }
 
+		/// <summary>
+		/// Adds details to tooltip help box. 
+		/// </summary>
+		/// <param name="details">Details text. Can be a regular string or resolved to method that returns string to display</param>
 		public TooltipDetailsAttribute(string details)
 		{
 			Details = details;
